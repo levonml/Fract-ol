181ad5c7d9a96a9e2abeab29e562d10f881acd66
Levon Stepanyan <levonmll@gmail.com>
2021-01-03 18:14:46 +0100

    mandtlbrot and julia, zoom in out, iteration managable

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 fractol:
-	clang  fractol.c  -I /usr/local/include -L /usr/local/lib/ -lmlx -I /usr/include/X11 -L /usr/lib/x86_64-linux-gnu -lXext -lX11 
+	make -C libft
+	clang  -g fractol.c  -L libft -lft -I /usr/local/include -L /usr/local/lib/ -lmlx -I /usr/include/X11 -L /usr/lib/x86_64-linux-gnu -lXext -lX11 
+clean:
+	make clean -C libft
+	rm -rf *~ *.o
--- fractol.h
+++ fractol.h
@@ -4,22 +4,26 @@
 # define COLOR 0xff0000
 # define Z 1
 # define ZOOM 15
-//# include "libft/libft.h"
+# include "libft/libft.h"
 # include "mlx.h"
 
 typedef struct		s_data
 {
 	int			x;
 	int			y;
+	int			x_pos;
+	int			y_pos;
 	int			color;
 	int			count;
 	int			pixel;
 	int			pixel_bits;
 	int			line_bytes;
 	int			endian;
+  	int			iter;
   //}					t_ints;
 //typedef	struct		s_floats
 //{
+	double			zoom;
 	float			zr;
 	float			zi;
   	float			cx;
@@ -30,5 +34,7 @@ typedef struct		s_data
 	void			*win;
 	void			*image;
 	char			*buffer;
+	char			*argv;
 }					t_data;
+int ft_strcmp(const char *s1, const char *s2);
 #endif
